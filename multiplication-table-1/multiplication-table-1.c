ad22cb3d04893cecc66ca3377b7d0a79b8872c5a
Vakhtang Tivadze <vaxotivadze@gmail.com>
2023-05-12 18:37:56 +0400

    multiplication table 1

---
+++ multiplication-table-1/multiplication-table-1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void printMultiplicationTable(int n);
+int acceptUserNumber();
+
+void main(void) {
+	int n;
+	
+	n = acceptUserNumber();
+	printMultiplicationTable(n);
+}
+
+int acceptUserNumber() {
+	int n;
+	char c;
+	
+	printf("Enter an integer from 1 to 9: ");
+	while (!scanf("%d", &n)) {
+		printf("Is not a number. Try again: ");
+		while ((c = getchar()) != '\n');
+	}
+	
+	return n;
+}
+
+void printMultiplicationTable(int n) {
+	int i = 1, j = 1;
+	
+	printf("\nThe multiplication table for a number %d:\n\n", n);
+	
+	while (i <= n) {
+		while (j <= n) {
+			printf("%2d\t", i * j);
+			j++;
+		}
+		i++;
+		j = 1;
+		printf("\n");
+	}
+}
